22d7f6b13ec6faf16f3551622af111c503e63dc4
Umar Rajput <47900850+umarraj008@users.noreply.github.com>
2025-03-09 20:14:08 +0000

    editing tests

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 test_data = [
+
+  # REGULAR POST CHECKS
     [
         "Regular Post",
 
@@ -19,10 +21,24 @@ test_data = [
         """,
         "7w1YgW5kkwBz43E32jWdnskK8wdvgvsSAoWZ7v9Nmoon",
         
-        "Hwercue3DeYXUhbybMrUeX8KNMh42zZQdhbMNN3wMwyc"
+        "7w1YgW5kkwBz43E32jWdnskK8wdvgvsSAoWZ7v9Nmoon"
     ],
     [
-      "Remove DEEZ -> URL",
+      "Regular message NO CA",
+
+      """
+      HEY guys so this coin just went 10000x and im gay so you should have in vested, because i am yeezus yeez and deez is my wife.
+      https://fartclubsol.com/
+      https://x.com/fartclub_ct
+      https://t.me/cto_fartclub
+      """,
+      
+      None
+    ],
+    
+    # REMOVE KING/WORD CHECKS
+    [
+      "Remove DEEZ quotes)",
 
         """
         🚨HIGH RISK HIGH REWARD GAMBLE PLAY🚨
@@ -31,7 +47,7 @@ test_data = [
 
         GOOD NARRATIVE FART META. THE ONLY OTHER FART BOOK MY ADAM WALLACE LIKE THE FARTBOY COIN  THAT RAN OVER 200M THE REST ARE JUST DERIVATIVES EXCEPT THIS ONE SO I THINK ITS SUPER UNDERVALUED. 300K MC AREA DYOR NFA.
 
-        https://dexscreenerDEEZ.com/solana/4iWpF4TMzHnDP7tjfYWqW8x1qWHb1AbqNhhJqVdTYxL4
+        https://dexscreener.com/solana/4iWpF4TMzHnDP7tjfYWqWDEEZ8x1qWHb1AbqNhhJqVdTYxL4
 
         https://fartclubsol.com/
 
@@ -44,11 +60,9 @@ test_data = [
         YOU HAVE TO REMOVE THE WORD “DEEZ” FROM THE CA AND DEX LINK SO BOTS GET SIDELINED
         """,
         "6zkZPeSVSynKoNgPjb6yCfJ5BFFro4gcKXuMrPtvpump",
-        
-        "4iWpF4TMzHnDP7tjfYWqW8x1qWHb1AbqNhhJqVdTYxL4"
     ],
     [
-      "Remove Deez -> CA",
+      "Remove Deez no quotes",
 
       """
       🚨HIGH RISK HIGH REWARD GAMBLE PLAY🚨
@@ -67,13 +81,13 @@ test_data = [
 
       6zkZPeSVSynKoNgPjb6DEEZyCfJ5BFFro4gcKXuMrPtvpump
 
-      YOU HAVE TO REMOVE THE WORD “DEEZ” FROM THE CA AND DEX LINK SO BOTS GET SIDELINED
+      YOU HAVE TO REMOVE THE WORD DEEZ FROM THE CA AND DEX LINK SO BOTS GET SIDELINED
       """,
 
       "6zkZPeSVSynKoNgPjb6yCfJ5BFFro4gcKXuMrPtvpump"
     ],
     [
-      "Remove KING -> URL",
+      "Remove KING quotes",
 
       """
       🚨HIGH RISK HIGH REWARD GAMBLE PLAY🚨
@@ -95,11 +109,9 @@ test_data = [
       YOU HAVE TO REMOVE THE WORD “KING” FROM THE CA AND DEX LINK SO BOTS GET SIDELINED
       """,
       "6zkZPeSVSynKoNgPjb6yCfJ5BFFro4gcKXuMrPtvpump",
-      
-      "4iWpF4TMzHnDP7tjfYWqW8x1qWHb1AbqNhhJqVdTYxL4"
     ],
     [
-      "Remove KING from middle -> URL",
+      "Remove KING no quotes",
 
       """
       🚨HIGH RISK HIGH REWARD GAMBLE PLAY🚨
@@ -118,37 +130,86 @@ test_data = [
 
       6zkZPeSVSynKoNgPjb6KINGyCfJ5BFFro4gcKXuMrPtvpump
 
-      YOU HAVE TO REMOVE THE WORD “KING” FROM THE CA AND DEX LINK SO BOTS GET SIDELINED
+      YOU HAVE TO REMOVE THE WORD KING FROM THE CA AND DEX LINK SO BOTS GET SIDELINED
       """,
       "6zkZPeSVSynKoNgPjb6yCfJ5BFFro4gcKXuMrPtvpump",
-      
-      "4iWpF4TMzHnDP7tjfYWqW8x1qWHb1AbqNhhJqVdTYxL4"
     ],
+    
+    # SPLIT CHECKS
     [
-      "Remove KING -> CA",
+      "Split CA",
 
       """
-      🚨HIGH RISK HIGH REWARD GAMBLE PLAY🚨
+      🚀 SUPERBOWL GAMBLE PLAY 🚀 
 
-      GOING OUT ACROSS ALL PLATFORMS
+      POSTING ACROSS ALL PLATFORMS SO IF YOU JEET YOURE GAY AND WILL STAY POOR 
 
-      GOOD NARRATIVE FART META. THE ONLY OTHER FART BOOK MY ADAM WALLACE LIKE THE FARTBOY COIN  THAT RAN OVER 200M THE REST ARE JUST DERIVATIVES EXCEPT THIS ONE SO I THINK ITS SUPER UNDERVALUED. 300K MC AREA DYOR NFA.
+      GOOD NARRATIVE AND BRANDING WITH THE MIX OF SUPERBOWL AND CRYPTO. THIS TEAM RAN STUFF INTO THE MILLIONS PRECIOUSLY. WILL HAVE BOOST AT BONDING WITH ADS ETC. DYOR NFA. 
 
-      https://dexsaslikfncreener.com/solana/4iWpF4TMzHnDP7tjfYDEEZNUTTYWqW8x1qWHbKING1AbqNhhJqVdTYxL4
+      https://x.com/SuperBull_SOL
 
-      https://fartclubsol.com/
+      https://t.me/SuperBull_Solana
 
-      https://x.com/fartclub_cto
+      https://dexscreener.com/solana/7ky51wxqrjkaxuitqwd1g6fqmzy9s2lcsbafie88kyoh
 
-      https://t.me/cto_fartclub
-
-      6zkZPeSVSynKoNgPjb6KINGyCfJ5BFFro4gcKXuMrPtvpump
-
-      YOU HAVE TO REMOVE THE WORD “KING” FROM THE CA AND DEX LINK SO BOTS GET SIDELINED
-      """,
+      CSChJMDH1drnxaN5ZXr8ZP
       
-      "6zkZPeSVSynKoNgPjb6yCfJ5BFFro4gcKXuMrPtvpump"
+      ZtqXv2FJqNTGcSujyfmoon
+
+      CA SPLIT INTO 2 TO SIDELINE BOTS
+      """,
+      "ZtqXv2FJqNTGcSujyfmoonZtqXv2FJqNTGcSujyfmoon",
     ],
+    [
+      "Split CA but only SPLIT word",
+
+      """
+      🚀 SUPERBOWL GAMBLE PLAY 🚀 
+
+      POSTING ACROSS ALL PLATFORMS SO IF YOU JEET YOURE GAY AND WILL STAY POOR 
+
+      GOOD NARRATIVE AND BRANDING WITH THE MIX OF SUPERBOWL AND CRYPTO. THIS TEAM RAN STUFF INTO THE MILLIONS PRECIOUSLY. WILL HAVE BOOST AT BONDING WITH ADS ETC. DYOR NFA. 
+
+      https://x.com/SuperBull_SOL
+
+      https://t.me/SuperBull_Solana
+
+      https://dexscreener.com/solana/7ky51wxqrjkaxuitqwd1g6fqmzy9s2lcsbafie88kyoh
+
+      CSChJMDH1drnxaN5ZXr8ZP
+      
+      ZtqXv2FJqNTGcSujyfmoon
+
+      SPLIT
+      """,
+      "ZtqXv2FJqNTGcSujyfmoonZtqXv2FJqNTGcSujyfmoon",
+    ],
+    [
+      "Split CA but odd splits",
+
+      """
+      🚀 SUPERBOWL GAMBLE PLAY 🚀 
+
+      POSTING ACROSS ALL PLATFORMS SO IF YOU JEET YOURE GAY AND WILL STAY POOR 
+
+      GOOD NARRATIVE AND BRANDING WITH THE MIX OF SUPERBOWL AND CRYPTO. THIS TEAM RAN STUFF INTO THE MILLIONS PRECIOUSLY. WILL HAVE BOOST AT BONDING WITH ADS ETC. DYOR NFA. 
+
+      https://x.com/SuperBull_SOL
+
+      https://t.me/SuperBull_Solana
+
+      https://dexscreener.com/solana/7ky51wxqrjkaxuitqwd1g6fqmzy9s2lcsbafie88kyoh
+
+      CSChJMDH1dr
+      
+      nxaN5ZXr8ZPZtqXv2FJqNTGcSujyfmoon
+
+      CA SPLIT INTO 2 TO SIDELINE BOTS
+      """,
+      "ZtqXv2FJqNTGcSujyfmoonZtqXv2FJqNTGcSujyfmoon",
+    ],
+
+    # RUG PULL CHECKS
     [
       "Rug Pull Message",
 
@@ -173,57 +234,6 @@ test_data = [
       """,
       
       "This coin is flagged as a rug pull. Stopping processing."
-    ],
-    [
-      "Remove Jeff -> URL",
-
-      """
-      🚨HIGH RISK HIGH REWARD GAMBLE PLAY🚨
-
-      GOING OUT ACROSS ALL PLATFORMS
-
-      GOOD NARRATIVE FART META. THE ONLY OTHER FART BOOK MY ADAM WALLACE LIKE THE FARTBOY COIN  THAT RAN OVER 200M THE REST ARE JUST DERIVATIVES EXCEPT THIS ONE SO I THINK ITS SUPER UNDERVALUED. 300K MC AREA DYOR NFA.
-
-      https://dexJEFFscreener.com/solana/4iWpF4TMzHnDP7tJEFFjfYWqW8x1qWHJEFFb1AbqNhhJqVdTYxL4
-
-      https://fartclubsol.com/
-
-      https://x.com/fartclub_cto
-
-      https://t.me/cto_fartclub
-
-      6zkZPeSJEFFVSynKoNgPjb6yCfJ5BJEFFFFro4gcKXuMrPtvpump
-
-      YOU HAVE TO REMOVE THE WORD “JEFF” FROM THE CA AND DEX LINK SO BOTS GET SIDELINED
-      """,
-      "6zkZPeSVSynKoNgPjb6yCfJ5BFFro4gcKXuMrPtvpump",
-      
-      "6zkZPeSVSynKoNgPjb6yCfJ5BFFro4gcKXuMrPtvpump"
-    ],
-    [
-      "Remove Jeff -> CA",
-
-      """
-      🚨HIGH RISK HIGH REWARD GAMBLE PLAY🚨
-
-      GOING OUT ACROSS ALL PLATFORMS
-
-      GOOD NARRATIVE FART META. THE ONLY OTHER FART BOOK MY ADAM WALLACE LIKE THE FARTBOY COIN  THAT RAN OVER 200M THE REST ARE JUST DERIVATIVES EXCEPT THIS ONE SO I THINK ITS SUPER UNDERVALUED. 300K MC AREA DYOR NFA.
-
-      https://dexJEFFsasffasfcreener.com/solana/4iWpF4ghdhgsdhsTMzHnDP7tJEFFjfYWqW8x1qWHb1AbqNhJaahJqVdTYxL4
-
-      https://fartclubsol.com/
-
-      https://x.com/fartclub_cto
-
-      https://t.me/cto_fartclub
-
-      6zkZPeSJEFFVSynKoNgPjb6yCfJ5BFFro4gcKXuMrPtvpump
-
-      YOU HAVE TO REMOVE THE WORD “JEFF” FROM THE CA AND DEX LINK SO BOTS GET SIDELINED
-      """,
-      
-      "6zkZPeSVSynKoNgPjb6yCfJ5BFFro4gcKXuMrPtvpump"
     ],
     [
       "Insta Rug Keyword",
@@ -275,28 +285,195 @@ test_data = [
       
       "This coin is flagged as a rug pull. Stopping processing."
     ],
+    # add frew more to check each keyword
+
+    # APED POSTS
     [
-      "Regular message NO CA",
+      "Aped Message 1",
 
       """
-      HEY guys so this coin just went 10000x and im gay so you should have in vested, because i am yeezus yeez and deez is my wife.
-      https://fartclubsol.com/
-      https://x.com/fartclub_ct
-      https://t.me/cto_fartclub
+      3bSYipM5TppKqLtANdgrmNdfpwUmkNzJdvw9nXtipump
+
+
+      Aped at 8k mc❤️💯
       """,
       
-      None
+      "3bSYipM5TppKqLtANdgrmNdfpwUmkNzJdvw9nXtipump"
+    ],
+    [
+      "Aped Message 2",
+
+      """
+      GZtPWooCmHsGmQ7qHCKDJU6LLFdjv6GLcUkPhiL8pump
+
+
+      Gambled at 7k mc❤️💯
+      """,
+      
+      "GZtPWooCmHsGmQ7qHCKDJU6LLFdjv6GLcUkPhiL8pump"
+    ],
+    [
+      "Aped Message 3",
+
+      """
+      HTRTxzNZLq3zHbzLvrSGppjJb7ruB4TivebqSc8Vpump
+
+
+      Aped at 10k mc❤️💯
+      """,
+      
+      "HTRTxzNZLq3zHbzLvrSGppjJb7ruB4TivebqSc8Vpump"
+    ],
+    
+    # YEEZUS POSTS
+    [
+      "Yeezus normal post: Mini Doge CA",
+
+      """
+      APED SOME $MINIDOGE AROUND 11.5M MC. 
+
+      TEAM ARE EARLY DOGE INVESTORS AND RAN PREVIOUS PROJECT TO 250M LIKE IT WAS NOTHING.
+
+      DOGE NARRATIVE X SOLANA REWARDS FOR HOLDING EVERY 4 MIN 20 SECONDS SO ITS ELON CODED. 
+
+      Something about this team and this token is incredibly bullish.
+
+      I heard they are onboarding more heavy hitters today.
+
+      GOING TO PUSH A VIDEO ON ALL PLATFORMS AS WELL NOW 
+
+      CA:
+      DSXVmrBySfBcmdNDGQkk59hGwXhAKNjEwc4as8nfmysd
+
+      Dexscreener:
+      https://dexscreener.com/solana/5dyma5yb1tqyxkctowbbvq6ahhyvr3jyxyn5k7wpplkq
+
+      TG:
+      https://t.me/TheSocietyPortal
+      """,
+      
+      "DSXVmrBySfBcmdNDGQkk59hGwXhAKNjEwc4as8nfmysd"
+    ],
+    [
+      "Yeezus Split Post",
+
+      """
+      🚨PAID PROMO ACROSS ALL PLATFORMS🚨
+
+      HIGH RISK HIGH REWARD GAMBLE PLAY DYOR NFA.
+
+      FART META HOT RIGHT NOW GOOD TICKER AND BRANDING. PUMPING ALREADY SO DONT FOMO TRY TO LOOK FOR ENTRY 700K MC AREA. DYOR NFA. 
+
+      https://www.fartdaddysol.com/
+
+      https://x.com/fartdaddysolcto
+
+      https://t.me/FARTDADDYPORTAL
+
+      59Vaf6t5viekuPJ1ehPL3h
+
+      RC8DXxPGLTFfM2t8tgpump
+
+      💥 CA ADDRESS SPLIT INTO 2 TO SIDELINE BOTS 💥
+      """,
+      
+      "59Vaf6t5viekuPJ1ehPL3hRC8DXxPGLTFfM2t8tgpump"
+    ],
+    [
+      "Yeezus remove king no quotes",
+
+      """
+      🚨HIGH RISK GAMBLE PLAY 🚨
+
+      COBRA $KAI JUST LAUNCHED ON MOONSHOT AND INSTABONDED SEEMS LIKE SOMETHING COOKING BEHIND THIS 80K MC AREA. 
+
+      BRANDING AND TICKER IS LIT. KARATE KID IS LITERALLY AN ICON AND 3RD MOST SEARCHED SPORTS MOVIE TO THIS DAY AND EVEN COBRA KAI SERIES IS #1 ON NETFLIX. THIS HAS A LOT OF NOSTALIGIA, AND VIRAL POTENTIAL BEHIND IT AND COULD DO NUMBERS. 
+
+      HIGH RISK HIGH REWARD SO DYOR NFA AND LOOK FOR ENTRY. 
+
+      https://dexscreener.com/solana/38UwrmRCPwy91392KING7qRcGuixENguBhFjtRFB7NkoPujY
+
+      https://x.com/cobrakais0l
+
+      https://t.me/CobraKaiS0L
+
+      8jpJ8peczYNZesmKING1vVKU2TA1QsL3WWDa6yuGrg5kmoon
+
+      KING IN CA TO DETER BOTS
+      """,
+      
+      "8jpJ8peczYNZesmKING1vVKU2TA1QsL3WWDa6yuGrg5kmoon"
+    ],
+    
+    # VALIDATE SOLANA CHECKS
+        [
+      "Correct CA should validate True",
+
+      """
+      DSXVmrBySfBcmdNDGQkk59hGwXhAKNjEwc4as8nfmysd
+      """,
+      
+      "DSXVmrBySfBcmdNDGQkk59hGwXhAKNjEwc4as8nfmysd"
+    ],
+    [
+      "CA with less than 44 chars",
+
+      """
+      DSXVmrBySfBcmdNDGQkk59hGwXhAK
+      """,
+      
+      "INCORRECT CA"
+    ],
+    [
+      "CA with more than 44 chars",
+
+      """
+      DSXVmrBySfBcmdNDGQkk59hGwXhAKNjEwc4as8nfmysdGQkk59hGwXhAK
+      """,
+      
+      "INCORRECT CA"
+    ],
+    [
+      "Incorrect CA should validate False",
+
+      """
+      DSXVmrBfSfBcmdNDGQka59hGwXhAKNjEwc4astnfmysd
+      """,
+      
+      "INCORRECT CA"
+    ],
+
+    # SAVED CA CHECK
+    [
+      "Saved CA should be skipped",
+
+      """
+      🚨LOW CAP HIGH RISK HIGH REWARD AI PLAY🚨
+
+      RUGX BUILDING SOME COOL STUFF AND I LIKE THE BRANDING AND TICKER. SPENT OVER 30K TO START DEVELOPING PRODUCT WHICH DOES SOLVE A PROBLEM AND IS ALSO REVENUE BASED TO HELP THEM SUSTAIN. 88M TOKENS LOCKED FOR VESTING BY TEAM SHOWING DEDICATION. I THINK AS LONG AS THIS SURVIVES TILL MONEY FLOWS BACK INTO AI PLAYS THIS COULD BE A HOME-RUN. SITTING AT 160K MARKETCAP AREA SO 100X+ FROM HERE ISNT HARD AT ALL LONG TERM. 
+
+      AGAIN LOW CAP HIGH RISK HIGH REWARD AI PLAY SO DYOR NFA 
+
+      https://www.rugx.ai/
+
+      https://x.com/rugxai
+
+      https://t.me/RUGxAI
+
+      https://dexscreener.com/solana/FaRGTYM4UHzu8xxKhcP5ixGNxBncniBZEzDjwnbS6Cnp
+
+      2eQEE6yWkAygKgwCeRq1aJcEkjBqnqkhGYo9SQDPpump
+      """,
+      
+      "Address 2eQEE6yWkAygKgwCeRq1aJcEkjBqnqkhGYo9SQDPpump has already been forwarded or is invalid. Skipping."
     ],
 ]
 
-#This coin is flagged as a rug pull. Stopping processing.
+  #   [
+  #     "DESCRIPTION",
 
-
-# [
-#   "",
-
-#   """
-#   """,
-  
-#   ""
-# ],
+  #     """
+  #     """,
+      
+  #     "EXPECTED"
+  #   ],
